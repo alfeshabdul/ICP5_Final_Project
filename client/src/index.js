f201e9b6bf06551460daaabe676e2c33e8f94687
hemantgaikwad13 <hsgaikwad0015@gmail.com>
2023-12-15 16:41:07 +0530

    women page added

--- client/src/index.js
+++ client/src/index.js
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
         "element":<Mensproduct/>
     },
     {
-        "path":"/womensproducts",
+        "path":"/womenswear",
         "element":<Womensproduct/>
     },
     {
